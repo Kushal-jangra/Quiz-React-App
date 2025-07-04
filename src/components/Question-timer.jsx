4ae717ff737312faeded5e0e97b7346fee600760
Kushal Jangra <connect2kushalj@gmail.com>
2025-07-04 20:58:10 +0530

    resolved mreged conflicts

--- src/components/Question-timer.jsx
+++ src/components/Question-timer.jsx
@@ -32,30 +32,3 @@ export default function QuestionTimer({ timer, onTimeout, mode, isPaused }) {
     />
   );
 }
-import { useEffect, useState } from "react";
-
-export default function QuestionTimer({ timer, onTimeout, mode }) {
-  const [remainingTime, setReamainingTime] = useState(timer);
-  useEffect(() => {
-    const Timer = setTimeout(onTimeout, timer);
-    return () => {
-      clearTimeout(Timer);
-    };
-  }, [timer, onTimeout]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setReamainingTime((prevTime) => prevTime - 10);
-    }, 10);
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <progress
-      id="question-timer"
-      value={remainingTime}
-      max={timer}
-      className={mode}
-    />
-  );
-}
